7ba45d285be80587939eea797453d0e2d06d109c
clienthax <clienthax@gmail.com>
2021-04-25 15:23:37 +0100

    Hack for msmw2.sprx timeout.

--- rpcs3/Emu/Cell/lv2/sys_event.cpp
+++ rpcs3/Emu/Cell/lv2/sys_event.cpp
@@ -268,6 +268,13 @@ error_code sys_event_queue_receive(ppu_thread& ppu, u32 equeue_id, vm::ptr<sys_e
 
 	ppu.gpr[3] = CELL_OK;
 
+	// "/dev_flash/vsh/module/msmw2.sprx" seems to rely on some cryptic shared memory behaviour that we don't emulate correctly
+	// This is a hack to avoid waiting for 1m40s every time we boot vsh
+	if (timeout == 0x5f5e100) {
+		sys_event.todo("sys_event_queue_receive(equeue_id=0x%x, *0x%x, timeout=0x%llx) Bypassing timeout for msmw2.sprx", equeue_id, dummy_event, timeout);
+		timeout = 1;
+	}
+
 	const auto queue = idm::get<lv2_obj, lv2_event_queue>(equeue_id, [&](lv2_event_queue& queue) -> CellError
 	{
 		if (queue.type != SYS_PPU_QUEUE)
